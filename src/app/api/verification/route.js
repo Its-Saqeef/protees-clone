1c5fb11ce697e0ad8231806dc551fd0da6136c08
Muhammad Saqeef <muhammadxaqeef@gmail.com>
2025-03-24 07:05:44 +0500

    Fixed Email

--- src/app/api/verification/route.js
+++ src/app/api/verification/route.js
@@ -1,6 +1,9 @@
 import connectDB from "@/lib/Connection";
 import { User } from "@/components/Backend/models/Users.models";
 import {sendEmail} from "@/lib/Mailer"
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req) {
     try {
@@ -23,8 +26,14 @@ export async function POST(req) {
             tokenExpiry : Date.now() + 900000
         })
 
-        const response = await sendEmail(email,code)
-       
+       // const response = await sendEmail(email,code)
+       const data=await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: "proteesclone@gmail.com",
+        subject: `${code} is your login code`,
+        html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+      })
+      console.log(data)
 
         return Response.json({
             message : "Success"
