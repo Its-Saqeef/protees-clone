7f8a168af84affd3216162c2e43c4d07fe06b333
Muhammad Saqeef <muhammadxaqeef@gmail.com>
2025-03-30 02:55:06 +0500

    Fixed URL

--- src/app/collections/[category]/product/[id]/page.js
+++ src/app/collections/[category]/product/[id]/page.js
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 
 async function GetData(id) {
   const getData = await axios
-    .get(`${NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/api/getproducts/${id}`)
+    .get(`${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/api/getproducts/${id}`)
     .then((res) =>res)
   return getData;
 }
